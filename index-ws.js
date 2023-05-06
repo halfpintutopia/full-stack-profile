ddc2eaff8cb08687c667566bfbc9cabfcd15511c
Sirinya Richardson <siri.richardson@gmail.com>
2023-05-06 10:41:15 +0200

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,16 @@ server.listen(3000, function logListeningMessage() {
     console.log('Listening on 3000');
 });
 
+// Signal interrupt with a process listener - be aware that it won't work if the websocket is not closed
+process.on('SIGINT', () => {
+    console.log('sigit');
+    wss.clients.forEach(function each(client) {
+       client.close();
+    });
+    server.close(() => {
+        shutdownDB();
+    });
+});
 
 /** Begin websocket */
 const WebSocketserver = require('ws').Server;
@@ -33,6 +43,8 @@ wss.on('connection', function connection(ws) {
         ws.send('Welcome to my server');
     }
 
+    db.run(`INSERT INTO visitors (count, time) VALUES (${numClients}, datetime('now'))`)
+
     ws.on('close', function close() {
         wss.broadcast(`Current visitors: ${numClients}`);
         console.log('A client has disonnected');
@@ -44,3 +56,34 @@ wss.broadcast = function broadcast(data) {
         client.send(data);
     });
 };
+
+/** end websockets */
+
+/** begin database */
+
+const sqlite = require('sqlite3');
+// const db = new sqlite.Database('./sirinyarichardson.db');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors
+        (
+            count INTEGER,
+            time  TEXT
+        )
+    `)
+});
+
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+        console.log(row);
+    });
+}
+
+function shutdownDB() {
+    getCounts();
+    console.log("Shutting down db")
+    db.close();
+}
